6e330a700591e7e1139509e3187d01ae0f46487a
Rewa Patil <>
2021-05-11 00:40:29 +0000

    code check

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ document.getElementById('enter').addEventListener('click', myFunction)
 function myFunction () {
   // get age and convert to integer
   const age = parseInt(document.getElementById('age').Value)
-  let day = document.getElementById('day').value
+  const day = document.getElementById('day').value
   // check if age is more than 12
   if ((age < 12 || age > 21) && (day === 'Tuesday' || day === 'Thursday')) {
     alert('You can get student pricing')
   } else {
-    alert ('You cannot get student pricing') }
+    alert ('You cannot get student pricing')
   }
+}

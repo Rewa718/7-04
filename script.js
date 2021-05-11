6740c51cd8917c2a72ff49ab9bc60e0be40d0804
Rewa Patil <>
2021-05-11 00:22:53 +0000

    code

--- script.js
+++ script.js
@@ -3,15 +3,13 @@ document.getElementById('enter').addEventListener('click',myFunction)
 
 function myFunction () {
   // get age and convert to integer
-  age = document.getElementById('age').Value
-  age = parseInt(age)
-
+const age = parseInt(document.getElementById('age').Value)
+var day = document.getElementById('day').value  
   // check if age is more than 12
-  if ((day == "Tuesday" || day == "Thursday")|| (age > 12 && age < 21)) {
-   document.getElementById('answer').innerHTML = 'You can get student pricing'
-    
+  if ((age < 12 || age > 21) && (day == "Tuesday" || day == "Thursday")) {
+    alert('You can get student pricing');
+
   } else {
-    document.getElementById('answer').innerHTML = 'You cannot get student pricing'
-   
+    alert ('You cannot get student pricing')
   }
-}
+  }
